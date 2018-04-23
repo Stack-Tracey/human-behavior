3c8befd59a940165836275d0c33e7c21cc4a5465
Mel <2budde@informatik.uni-hamburg.de>
2018-04-23 15:29:33 +0200

    added graph

---
+++ agent/graph.py
@@ -0,0 +1,9 @@
+class Graph():
+
+    def __init__(self, node):
+        self.node = node
+
+
+# Returns an array of connections (of class connection.py) outgoing from the given node
+    def getConnections(self):
+        return null
