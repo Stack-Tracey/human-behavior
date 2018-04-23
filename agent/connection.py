516ae64cb29a10c1061964ff80bc3f3f6b05cc4b
Mel <2budde@informatik.uni-hamburg.de>
2018-04-23 15:28:50 +0200

    added connection

---
+++ agent/connection.py
@@ -0,0 +1,14 @@
+
+# Returns the non-negative cost of the connection
+class Connection():
+
+    def getCost():
+        return NULL
+
+# Returns the node that this connection came from
+    def getFromNode():
+        return null
+
+# Returns the node that this connection leads to
+    def getToNode():
+        return null
