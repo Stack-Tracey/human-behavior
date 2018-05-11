24859ba8ec4868415eb24cd5c59562c0a7956111
Mel <2budde@informatik.uni-hamburg.de>
2018-05-11 15:36:20 +0200

    added game

---
+++ agent/game.py
@@ -0,0 +1,20 @@
+
+class Game:
+    def __init__(self, frame):
+        self.frame = frame
+
+    def response(self, frame):
+        return None
+
+    def experiment_def(self, frame):
+        nr_of_trials = frame["Nr_of_Trials"]
+
+
+    def trial_def(self, frame):
+        return None
+
+    def play(self, frame):
+        return None
+
+    def start(self, frame):
+        return None
