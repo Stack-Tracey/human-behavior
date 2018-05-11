be828908ddc40a42c7840d28f4b257e541f2525f
Mel <2budde@informatik.uni-hamburg.de>
2018-05-11 15:40:45 +0200

    stuff

--- agent/graph.py
+++ agent/graph.py
@@ -1,3 +1,5 @@
+import connection
+
 class Graph():
 
     def __init__(self, node):
@@ -7,3 +9,22 @@ class Graph():
 # Returns an array of connections (of class connection.py) outgoing from the given node
     def getConnections(self):
         return null
+
+
+
+# Returns the non-negative cost of the connection
+#class Connection():
+
+    def getCost():
+        return NULL
+
+# Returns the node that this connection came from
+    def getFromNode():
+        return null
+
+# Returns the node that this connection leads to
+    def getToNode():
+        return null
+
+
+a = connection.get_trial_def()
