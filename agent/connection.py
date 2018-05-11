62f07d13ac6ffaaafa533b4b665d71ae82e82edf
Mel <2budde@informatik.uni-hamburg.de>
2018-05-11 15:35:19 +0200

    added funktions for selection

--- agent/connection.py
+++ agent/connection.py
@@ -1,23 +1,47 @@
 import labViewConnector
-# Returns the non-negative cost of the connection
-class Connection():
+import game
+trial_def = []
+play = []
+experiment_def = []
+overhead = []
+stream = labViewConnector.LabViewConnector('192.168.56.101', 1337)
+game = game.Game(stream)
 
-    def getCost():
-        return NULL
+def get_data(stream):
+    x = True
+    while x:
+        frame = stream.receive_fr()
+        msg_type = frame["MsgType"]
+        if msg_type == "Frame":
+            play.append(frame)
+            game.play(frame)
+            print("package received")
+            print(play)
+        elif msg_type == "Experiment Definition":
+            experiment_def.append(frame)
+            game.experiment_def(frame)
+        elif msg_type == "Trial Definition":
+            trial_def.append(frame)
+            game.trial_def(frame)
+            print("package received")
+            print(trial_def)
+        elif msg_type == "Start":
+            #game.start(frame)
+            print(frame)
+        elif msg_type == "End":
+            x = False
+            return x
+            get_data(stream)
+        else:
+            overhead.append(a)
+            #b = game.response() # placeholder for calculating stuff
+            #con.send_fr(b)
+    get_data(stream)
 
-# Returns the node that this connection came from
-    def getFromNode():
-        return null
-
-# Returns the node that this connection leads to
-    def getToNode():
-        return null
-
-
-con = labViewConnector.LabViewConnector('192.168.56.101', 1337)
-while True:
-    a = con.receive_fr()
-    b = {'x' : 5} #placeholder for calculating stuff
-    con.send_fr(b)
-
-con.close()
+get_data(stream)
+print("package received:")
+print(overhead)
+print(experiment_def)
+print(trial_def)
+print(play)
+stream.close()
