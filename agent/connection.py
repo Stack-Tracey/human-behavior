c97bbb80cfd996776e5b84e90ecfb803fe94ab8b
Mel <2budde@informatik.uni-hamburg.de>
2018-05-21 15:53:52 +0200

    rmv loop

--- agent/connection.py
+++ agent/connection.py
@@ -1,10 +1,12 @@
 import labViewConnector
 import game
+import time
 
-trial_defs = []
 play = []
-experiment_def = []
 overhead = []
+trial_def = []
+experiment_def = []
+
 stream = labViewConnector.LabViewConnector('192.168.56.101', 1337)
 game = game.Game(stream)
 
@@ -25,24 +27,25 @@ def get_data(stream):
             #print("package received")
             #print(experiment_def)
         elif msg_type == "Trial Definition":
-            trial_defs.append(frame)
+            game.trial_def(frame)
+            trial_def.append(frame)
             #print("package received")
             #print(trial_defs)
         elif msg_type == "Start":
             game.start(frame)
             #print("package received")
             #print(frame)
+        elif msg_type == "Stop": #TODO handling of pause option
+            time.sleep(5)
+            #print("package received")
+            #print(frame)
         elif msg_type == "End":
             x = False
             return x
             #get_data(stream)
-        elif msg_type == "Frame" and frame['Frame Data']['Frame Nr.'] == 0:
-            print("sorting works")
-            print(trial_defs)
-            game.trial_def(trial_defs)
-            game.play(frame)
-            play.append(frame)
         else:
+            print("overhead has been used---------------------------------------------------------------")
+            print(frame)
             overhead.append(frame)
             #b = game.response() # placeholder for calculating stuff
             #con.send_fr(b)
@@ -51,9 +54,5 @@ def get_data(stream):
 
 
 get_data(stream)
-#print("package received:")
-#print(overhead)
-#print(experiment_def)
-#print(trial_defs)
-#print(play)
+
 stream.close()
--- agent/trialState.py
+++ agent/trialState.py
@@ -4,7 +4,7 @@ class Graph:
     def __init__(self, obs_x, obs_y, obs_z, obs_x_size, obs_y_size, obs_z_size, obs_z_angle_deg, obs_slowdown_fac,
                   obs_visibility, obs_geometric_type,
                   target_x, target_y, target_z, target_z_size, target_radius, ball_x, ball_y, ball_z,
-                  ball_radius, nr_of_targets):
+                  ball_radius, nr_of_targets, nr_of_obstacles):
         self.obs_x = obs_x
         self.obs_y = obs_y
         self.obs_z = obs_z
@@ -25,6 +25,33 @@ class Graph:
         self.ball_z = ball_z
         self.ball_radius = ball_radius
         self.nr_of_targets = nr_of_targets
+        self.nr_of_obstacles = nr_of_obstacles
+
+        def get_objects(obj_x, obj_y, obj_z, amount):
+
+            objects = []
+            i = 0
+
+            while i < amount:
+                x_val = obj_x[i]
+                y_val = obj_y[i]
+                z_val = obj_z[i]
+
+                obj = []
+                obj.append(x_val)
+                obj.append(y_val)
+                obj.append(z_val)
+                objects.append(obj)
+                print(obj)
+                print(objects)
+                i = i + 1
+
+            return objects
+
+        self.obstacles = get_objects(obs_x, obs_y, obs_z, nr_of_obstacles)
+        self.targets = get_objects(target_x, target_y, target_z, nr_of_targets)
 
     def pause(self):
         return None
+
+
