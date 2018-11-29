cb35d0f89380be929ae47422104a1f6068a3a8d4
Mel <2budde@informatik.uni-hamburg.de>
2018-11-29 18:14:20 +0100

    enabled profiling

--- client/socket/labViewConnector.py
+++ client/socket/labViewConnector.py
@@ -7,24 +7,26 @@ class LabViewConnector:
         self.sock.connect((ip, port))
 
     def receive_fr(self):
-        byteA = bytearray()
-        while True:
-            data = self.sock.recv(1)
-            if data[0] == 10:
-                break
-            else:
-                byteA.append(data[0])
-        print(byteA)
-        str = byteA.decode("utf-8")
-        a = json.loads(str)
-        return a
+        try:
+            byteA = bytearray()
+            while True:
+                data = self.sock.recv(1)
+                if data[0] == 10 or data[0] == '\n':
+                    break
+                else:
+                    byteA.append(data[0])
+            #print(byteA)
+            str = byteA.decode("utf-8")
+            a = json.loads(str)
+            return a
+        except:
+            return json.loads('{"MsgType": "End"}')
 
     def send_fr(self, obj):
         str = json.dumps(obj).encode()
         str += b"\r\n"
-        print(str)
+        #print(str)
         self.sock.send(str)
 
     def close(self):
         self.sock.close()
-        file.close()
