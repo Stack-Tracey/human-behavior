ff0ad341aa38541d175ffe753347bbfa5fd33863
Mel <2budde@informatik.uni-hamburg.de>
2018-11-29 18:09:51 +0100

    enabeld profiling

--- testServer/main.py
+++ testServer/main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from pygame.locals import *
 from testServer.game import game
 from testServer.game import models
@@ -11,7 +12,9 @@ def main():
 
     for pos in ((512, 691), (246, 230), (778, 230), (512, 384)):
         Game.addTarget(pos)
-    for opt in ([(512, 445), 90], [(459, 353), 210], [(725, 261),330], [(420, 544),20], [(328, 384),100], [(420, 224),80], [(512, 77),180], [(604, 544),280], [(778, 538),300]):
+    for opt in (
+        [(512, 445), -90], [(459, 353), -210], [(725, 261), -330], [(420, 544), -20], [(328, 384), -100], [(420, 224), -80],
+        [(512, 77), -180], [(604, 544), -280], [(778, 538), -300]):
         Game.addObstacle(opt[0], opt[1])
 
     Game.server.waitForClient()
@@ -21,35 +24,73 @@ def main():
 
 
 def mainLoop(Game):
-    while True:
+    #pdb.Pdb().set_trace()
+    RUN = True
+    while RUN:
         receive = Game.server.receive_fr()
-        #p1_x = #TODO finding x coordinates from ball position
-        #p1_y =
         p1_fx = round(receive["Frame Data"]["X"]) #0.334566544444
         p1_fy = round(receive["Frame Data"]["Y"]) #0.334566544444
-
         Game.player.applyForces(p1_fx, p1_fy)
-        print("applied forces from peer:", p1_fx, p1_fy)
-        #Game.server.send_fr(Game.server.frame)
-        #print("send frame by server: ", Game.server.getFrame())
 
         events = pygame.event.get()
         for event in events:
             if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-                pygame.quit()
-                sys.exit()
+                RUN = False
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    Game.player.keys["K_LEFT"] = 1
+                elif event.key == K_RIGHT:
+                    Game.player.keys["K_RIGHT"] = 1
+                elif event.key == K_UP:
+                    Game.player.keys["K_UP"] = 1
+                elif event.key == K_DOWN:
+                    Game.player.keys["K_DOWN"] = 1
+            elif event.type == KEYUP:
+                if event.key == K_LEFT:
+                    Game.player.keys["K_LEFT"] = 0
+                elif event.key == K_RIGHT:
+                    Game.player.keys["K_RIGHT"] = 0
+                elif event.key == K_UP:
+                    Game.player.keys["K_UP"] = 0
+                elif event.key == K_DOWN:
+                    Game.player.keys["K_DOWN"] = 0
+        Game.player.applyKeys()
 
         pos = Game.player.rect.center
-        print("pos: ", pos)
         Game.server.p1_x, Game.server.p1_y = Game.server.p2_x, Game.server.p2_y = pos
-        print(" p1_x", Game.server.p1_x)
         Game.server.updateFrame()
         Game.server.send_fr(Game.server.frame)
-        print("send frame by server: ", Game.server.getFrame())
+
         Game.update(events)
         Game.draw(p1_fx, p1_fy)
         pygame.display.flip()
-        Game.clock.tick(60)
+        Game.clock.tick(30)
+
 
 if __name__ == "__main__":
-    main()
+    if "profile" in sys.argv:
+        import hotshot
+        import hotshot.stats
+        import tempfile
+        import os
+
+        print("TEst")
+        profile_data_fname = tempfile.mktemp("prf")
+        prof = hotshot.Profile(profile_data_fname)
+        prof.run('main()')
+        del prof
+        s = hotshot.stats.load(profile_data_fname)
+        s.strip_dirs()
+        print("cumulative\n\n")
+        s.sort_stats('cumulative').print_stats()
+        print("By time.\n\n")
+        s.sort_stats('time').print_stats()
+        del s
+        # clean up the temporary file name.
+        try:
+            os.remove(profile_data_fname)
+        except:
+            # may have trouble deleting ;)
+            pass
+    else:
+        main()
